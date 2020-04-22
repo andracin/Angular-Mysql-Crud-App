c5fd4a39de0342fc002d5143bb89700680e660e5
Andres Racines Bolaños <andracin>
2020-04-22 16:58:19 -0500

    app crud final

---
+++ database/db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE  database_links;
+
+USE database_links;
+
+CREATE TABLE  users(
+    id INT(11) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE users
+       ADD PRIMARY KEY (id);
+
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT 2 ;
+
+DESCRIBE users;
+
+--/ TABLA DE LINK
+
+CREATE TABLE links(
+    id INT(11) NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    descrition TEXT,
+    user_id  INT(11),
+    created_at TIMESTAMP NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN  KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE links
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT , AUTO_INCREMENT - 2;
---
+++ src/lib/passport.js
@@ -0,0 +1,54 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const pool = require('../database');
+const helpers = require('../lib/helpers')
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField :'username',
+    passwordField: 'password',
+    passReqToCallback: true
+},async (req, username,  password, done) =>{
+    const rows =await  pool.query('SELECT  * FROM users WHERE username=? ', [username]);
+    if (rows.length > 0) {
+         const user= rows[0];
+         const  validPassword = await helpers.matchPasword(password, user.password);
+         if (validPassword) {
+             done(null, user, req.flash('seccess','Bienvenido'+ user.username));
+         }else {
+             done(null, false, req.flash('message','Password Incorrecto'));
+         }
+    }else {
+        return done(null, false, req.flash('message', 'EL usuario no existe'));
+    }
+}));
+
+passport.use('local.signup', new LocalStrategy({
+    usernameField : 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, username,  password, done) =>{
+    const {fullname} =  req.body;
+    const newUser = {
+        username,
+        password,
+        fullname
+
+    };
+    newUser.password = await helpers.encryptPassword(password);
+    const result = await  pool.query('INSERT INTO users set ?', [newUser]);
+    newUser.id = result.insertId;
+    return done(null, newUser);
+   
+
+}));
+
+ passport.serializeUser((user, done) => {
+     done(null, user.id);
+
+});
+
+passport.deserializeUser( async (id ,done ) =>{
+    const rows = await pool.query('SELECT * FROM users WHERE id = ? ', [id]);
+    done(null, rows[0]);
+
+})
