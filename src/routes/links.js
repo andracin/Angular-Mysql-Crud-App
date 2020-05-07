9fa4a224fcd09ee2b1804023e5bfc07e7864d8d5
Andres Racines Bolaños <andracin@gmail.com>
2020-05-06 19:47:01 -0500

    git https://github.com/andracin/Angular-Mysql-Crud-App.git

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,29 +1,37 @@
+// @ts-nocheck
 const express =  require('express');
 const router = express.Router();
 const pool = require('../database')
 const {isLoggedIn , isNotLoggedIn}= require('../lib/auth');
+
+
 
 router.get('/add', isLoggedIn,  (req , res) => {
     res.render('./links/add');
 });
 
 router.post('/add', isLoggedIn , async (req , res) => {
-    const {title, url, descrition} = req.body;
+    const {title, url, descrition, user_id } = req.body;
     const newLink = {
         title,
         url,
-        descrition
+        descrition,
+        user_id
     };
     await pool.query('INSERT INTO links set ?', [newLink]);
     req.flash('success', 'Link Guardado Sactifactoriamente')
+    //console.log(user_id);
     res.redirect('/links');
 });
 
-router.get('/', isLoggedIn ,async (req, res) => {
-  const links =  await pool.query('SELECT * FROM links');
-  console.log(links);
-  res.render('./links/list', {links});
+router.get('/',  async ( req, res) => {
+    const user_id = req.user.id;
+    //console.log(user_id);
+    const links =  await pool.query('SELECT * FROM links WHERE user_id =?', user_id);
+    res.render('./links/list',{links});
 });
+
+ 
 router.get('/delete/:id', isLoggedIn,  async (req, res) => {
     const {id} =  req.params;
     await pool.query('DELETE FROM links  WHERE ID = ?',  [id]);
